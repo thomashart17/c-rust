425ea478a07646bf822e5b55a5dbb1a0bca0c465
GuyBlumenthal <guy.blumenthal@uwaterloo.ca>
2023-02-12 23:02:33 -0500

    Updated test lib to contain add function

--- src/test-lib/inc/lib.h
+++ src/test-lib/inc/lib.h
@@ -10,4 +10,6 @@ void test_func(void);
 
 void priv_func(void);
 
+int32_t add(int32_t x, int32_t y);
+
 #endif /* TEST_LIB_H_ */
--- src/test-lib/src/lib.rs
+++ src/test-lib/src/lib.rs
@@ -8,3 +8,8 @@ pub extern "C" fn test_func() {
 pub extern "C" fn priv_func() {
     println!("Rust in C");
 }
+
+#[no_mangle]
+pub extern "C" fn add(x: i32, y: i32) -> i32 {
+    x + y
+}

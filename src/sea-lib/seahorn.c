6cdc095d299d20a02961db01aa49d89f08d2f720
Arie Gurfinkel <arie.gurfinkel@uwaterloo.ca>
2023-05-26 14:34:18 -0400

    chore: comments

--- src/sea-lib/seahorn.c
+++ src/sea-lib/seahorn.c
@@ -1,3 +1,10 @@
+/**
+ * \brief Dummy implementation of SeaHorn verification builtins
+ *
+ * Dummy implementation is necessary to build Rust project with LTO.
+ * The bodies of these functions are removed by SeaHorn before any 
+ * optimizataion and verification takes place 
+ */
 #include "seahorn/seahorn.h"
 
 void __VERIFIER_error (void) {

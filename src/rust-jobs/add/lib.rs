4bbf3bba91ea3cb317a99f806ead2b24d3fa7a29
Arie Gurfinkel <arie.gurfinkel@uwaterloo.ca>
2023-05-26 23:24:12 -0400

    feat: convert add job to be pure Rust

--- src/rust-jobs/add/lib.rs
+++ src/rust-jobs/add/lib.rs
@@ -1,5 +1,19 @@
+use sea;
+
+
 #[no_mangle]
-pub extern "C" fn add(x: i32, y: i32) -> i32 {
-    // println!("Hi");
+pub extern "C" fn entrypt() {
+  let v = sea::nd_i32();
+  sea::assume(v >= 1);
+  let res = add(v, 7);
+  if v > 0 {
+    sea::sassert!(res > 7);
+  } else {
+    sea::sassert!(res <= 7);
+  }
+}
+
+#[no_mangle]
+fn add(x: i32, y: i32) -> i32 {
     x + y
 }

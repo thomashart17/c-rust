cc31ce161f5b2e4db6e5450aef3d9190a6c5d26a
Arie Gurfinkel <arie.gurfinkel@uwaterloo.ca>
2023-05-26 23:24:12 -0400

    feat: convert add job to be pure Rust

--- src/rust-jobs/add/add.c
+++ src/rust-jobs/add/add.c
@@ -1,24 +1,7 @@
-#include <stdio.h>
-
-#include "seahorn/seahorn.h"
-
 #include "inc/lib.h"
 
-int sea_nd_int(void) {
-    return 42;
-}
-
 int main() {
-    int v = sea_nd_int();
-    assume(v >= 1);
-    int res = add(v, 7);
-    printf("Result: %d\r\n", res);
-
-    if (v > 0) {
-        sassert(res > 7);
-    } else {
-        sassert(res <= 7);
-    }
-
-    return 42;
+  entrypt();
+  return 0;
 }
+

78c6d712be4b05a6ddde08e5f8f5c82c945d7b70
Arie Gurfinkel <arie.gurfinkel@uwaterloo.ca>
2023-05-28 10:17:14 -0400

    feat(sea-lib): Add define_sea_nd macro
    The macro should be used to define local sea_nd functions.
    
    It attemtps to create complex enough body of the function to confuse
    Rust optimizer from doing any intra-procedural optimizations.

--- src/sea-lib/src/seahorn.rs
+++ src/sea-lib/src/seahorn.rs
@@ -16,8 +16,9 @@ pub fn nd_i32() -> i32 {
 }
 
 #[no_mangle]
-#[inline(never)]
-pub fn nd2_i32() -> i32 { 42 }
+pub fn nd_bool() -> bool {
+  unsafe { bindings::sea_nd_bool() }
+}
 
 #[macro_export]
 macro_rules! sassert {
@@ -28,4 +29,36 @@ macro_rules! sassert {
     }}
 }
 
-
+/// Defines `sea_nd` function that returns nd value
+///
+/// The implementation generates a sea_nd function that
+/// attempts to confuse Rust optimizer from understanding what
+/// is the return of the function.
+///
+/// The first argument specifies the defined function name.
+/// It must start with the prefix `sea_nd`.
+///
+/// The second argument is the type for nondet values. The type
+/// is assumed to implement the Default trait
+///
+/// The third argument is a value to return in the implementation.
+/// It must be any value that is different from default value for the type.
+///
+/// # Example
+///
+/// define_seaa_nd!(sea_nd_foo, i32, 42);
+///
+#[macro_export]
+macro_rules! define_sea_nd {
+    ($name:ident,$typ:ty,$val:expr) => {
+        #[no_mangle]
+        #[inline(never)]
+        pub extern "C" fn $name() -> $typ {
+            if sea::nd_bool() {
+                <$typ>::default()
+            } else {
+                $val
+            }
+        }
+    };
+}

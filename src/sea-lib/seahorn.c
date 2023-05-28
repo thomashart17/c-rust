e3d07e7c7726a73d13f4041180a60b88d95cf3d1
Arie Gurfinkel <arie.gurfinkel@uwaterloo.ca>
2023-05-28 10:16:20 -0400

    feat(sea-lib): add sea_nd_bool

--- src/sea-lib/seahorn.c
+++ src/sea-lib/seahorn.c
@@ -2,24 +2,19 @@
  * \brief Dummy implementation of SeaHorn verification builtins
  *
  * Dummy implementation is necessary to build Rust project with LTO.
- * The bodies of these functions are removed by SeaHorn before any 
- * optimizataion and verification takes place 
+ * The bodies of these functions are removed by SeaHorn before any
+ * optimizataion and verification takes place
  */
 #include "seahorn/seahorn.h"
-#include <stdint.h> 
+#include <stdbool.h>
+#include <stdint.h>
 
-void __VERIFIER_error (void) {
-    return;
-}
+void __VERIFIER_error(void) { return; }
 
-void __VERIFIER_assume (int i) {
-    return;
-}
+void __VERIFIER_assume(int i) { return; }
 
-void __SEA_assume(bool b) {
-    return;
-}
+void __SEA_assume(bool b) { return; }
 
-int32_t sea_nd_i32(void) {
-  return 0;
-}
+int32_t sea_nd_i32(void) { return 0; }
+
+bool sea_nd_bool(void) { return true; }

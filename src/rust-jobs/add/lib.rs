d7eb1c486d6e5ee5b541744ce4b45e2a4d9836b8
Arie Gurfinkel <arie.gurfinkel@uwaterloo.ca>
2023-05-28 10:18:55 -0400

    chore(jobs): switched add to use local sea_nd

--- src/rust-jobs/add/lib.rs
+++ src/rust-jobs/add/lib.rs
@@ -1,18 +1,22 @@
 use sea;
 
+// Define a sea_nd function
+sea::define_sea_nd!(sea_nd_arg, i32, 42);
 
+// Entry point for the proof
 #[no_mangle]
 pub extern "C" fn entrypt() {
-  let v = sea::nd_i32();
-  sea::assume(v >= 1);
-  let res = add(v, 7);
-  if v > 0 {
-    sea::sassert!(res > 7);
-  } else {
-    sea::sassert!(res <= 7);
-  }
+    let v = sea_nd_arg();
+    sea::assume(v >= 1);
+    let res = add(v, 7);
+    if v > 0 {
+        sea::sassert!(res > 7);
+    } else {
+        sea::sassert!(res <= 7);
+    }
 }
 
+// Function being verified
 #[no_mangle]
 fn add(x: i32, y: i32) -> i32 {
     x + y

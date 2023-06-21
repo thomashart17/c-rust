a5d379665a5c485d0e377f25c7067e1d18cfc663
Boris <bjancic@uwaterloo.ca>
2023-06-21 12:01:49 -0400

    feat: added job for custom vec using raw vec
    Note that using this method, iter drop() can only be verified
    for up to 6 elements

--- src/rust-jobs/custom-vec-final/lib.rs
+++ src/rust-jobs/custom-vec-final/lib.rs
@@ -4,40 +4,45 @@
 use sea;
 
 extern crate alloc;
-use alloc::alloc::{Layout, alloc, realloc, dealloc, handle_alloc_error};
+use alloc::alloc::{Layout, alloc, dealloc, realloc, handle_alloc_error};
 use alloc::boxed::Box;
 
 use core::mem;
-use core::mem::ManuallyDrop;
+// use core::mem::ManuallyDrop;
 use core::ops::{Deref, DerefMut};
 use core::ptr::NonNull;
 use core::ptr;
 
 
+
 #[no_mangle]
 pub extern "C" fn entrypt() {
-    // test_new();
-    // sea::sassert!(false);
-    // test_grow();
-    // test_pop();
-    // test_push();
-    // test_drop();
-    // test_deref();
-    // test_deref_mut();
-    // test_insert();
-    // test_remove();
+    test_new();
+    test_grow();
+    test_pop();
+    test_push();
+    test_drop();
+    test_deref();
+    test_deref_mut();
+    test_insert();
+    test_remove();
 
-    test_into_iter();
-    // test_into_iter_size();
-    // test_into_iter_drop();
+    test_into_iter_front();
+    test_into_iter_back();
+    test_into_iter_size();
+    test_into_iter_drop();
+
+
+    // sea::sassert!(false);
 }
 
 #[no_mangle]
 fn test_new() {
     let v: CustomVec<i32> = CustomVec::new();
+    sea::sassert!(custom_vec_valid_after_init(&v));
     sea::sassert!(v.len == 0);
-    sea::sassert!(v.cap == 0);
-    sea::sassert!(!v.ptr.as_ptr().is_null());
+    sea::sassert!(v.cap() == 0);
+    sea::sassert!(!v.ptr().is_null());
 }
 
 #[no_mangle]
@@ -45,15 +50,17 @@ fn test_grow() {
     let original = sea::nd_usize();
 
     let mut v: CustomVec<i32> = CustomVec::new();
-    v.len = original;
-    v.cap = original;
+    sea::sassert!(custom_vec_valid_after_init(&v));
 
-    v.grow();
+    v.len = original;
+    v.buf.cap = original;
+
+    v.buf.grow();
 
     if original == 0 {
-        sea::sassert!(v.cap == 1)
+        sea::sassert!(v.cap() == 1)
     } else {
-        sea::sassert!(v.cap == 2 * original);
+        sea::sassert!(v.cap() == 2 * original);
     }
     sea::sassert!(v.len == original);
 }
@@ -64,14 +71,16 @@ fn test_pop() {
     sea::assume(original > 0);
 
     let mut v: CustomVec<i32> = CustomVec::new();
-    v.len = original;
-    v.cap = original;
+    sea::sassert!(custom_vec_valid_after_init(&v));
 
-    v.grow();
+    v.len = original;
+    v.buf.cap = original;
+
+    v.buf.grow();
     v.pop();
 
     sea::sassert!(v.len == original - 1);
-    sea::sassert!(v.cap == original * 2);
+    sea::sassert!(v.cap() == original * 2);
 }
 
 #[no_mangle]
@@ -80,18 +89,19 @@ fn test_push() {
     sea::assume(original > 0);
 
     let mut v: CustomVec<i32> = CustomVec::new();
+    sea::sassert!(custom_vec_valid_after_init(&v));
+
     v.len = original;
-    v.cap = original;
+    v.buf.cap = original;
 
-    v.grow();
+    v.buf.grow();
     v.push(0);   
-
     sea::sassert!(v.len == original + 1);
-    sea::sassert!(v.cap == original * 2);
+    sea::sassert!(v.cap() == original * 2);
+    // sea::sassert!(false);
 }
 
 #[no_mangle]
-#[inline(never)]
 fn test_drop() {
     pub struct DropTest { _value: i32, }
     impl Drop for DropTest {
@@ -175,25 +185,33 @@ fn test_remove() {
 }
 
 #[no_mangle]
-fn test_into_iter() {
+#[inline(never)]
+fn test_into_iter_front() {
     let n: u32 = 5;
-    // iterate forwards
-    let mut v: CustomVec<u32> = CustomVec::new();
-    for i in 0..n { v.push(i); }
-    let mut iter: IntoIter<u32> = v.into_iter();
 
+    let mut v: CustomVec<u32> = CustomVec::new();
     for i in 0..n {
-        sea::sassert!(iter.next().unwrap() == i);
+        v.push(i);
+    } 
+
+    let mut iter: IntoIter<u32> = v.into_iter();
+    for i in 0..n {
+        sea::sassert!(iter.next() == Some(i));
     }
+}
 
-    let n: u32 = 3;
-    // iterate backwards
+#[no_mangle]
+fn test_into_iter_back() {
+    let n: u32 = 5;
+
     let mut v: CustomVec<u32> = CustomVec::new();
-    for i in 0..n { v.push(i); }
-    let mut iter: IntoIter<u32> = v.into_iter();
-
     for i in 0..n {
-        sea::sassert!(iter.next_back().unwrap() == n-i-1);
+        v.push(i);
+    } 
+
+    let mut iter: IntoIter<u32> = v.into_iter();
+    for i in 0..n {
+        sea::sassert!(iter.next_back() == Some(n-i-1));
     }
 }
 
@@ -226,69 +244,97 @@ fn test_into_iter_drop() {
         fn drop(&mut self) { unsafe { DROP_COUNT += 1; } }
     }
 
-    let n: u32 = 24;
     let mut v: CustomVec<DropTest> = CustomVec::new();
-    for i in 0..n {
+    v.push(DropTest { _value: 0 });
+    for i in 0..4{
         v.push(DropTest { _value: i.try_into().unwrap() });
     }
+    v.push(DropTest { _value: 0 });
 
     let mut iter: IntoIter<DropTest> = v.into_iter();
-    let x: u32 = 0;
-    for _i in 5..x {
-        let front: bool = sea::nd_bool();
-        if front { _ = iter.next(); }
-        else { _ = iter.next_back(); }
-    }
+    iter.next();
+    iter.next();
+    iter.next_back();
+    iter.next_back();
 
-    sea::sassert!(unsafe { DROP_COUNT == x });
+    sea::sassert!(unsafe { DROP_COUNT == 4 });
 
     drop(iter);
-    sea::sassert!(unsafe { DROP_COUNT == n });
+    sea::sassert!(unsafe { DROP_COUNT == 6 });
 }
 
 
+fn custom_vec_valid_after_init<T>(vec: &CustomVec<T>) -> bool {
+    vec.len == 0 &&
+    vec.buf.cap == 0 && {
+        if mem::size_of::<T>() != 0 { vec.ptr() == NonNull::dangling().as_ptr() } 
+        else { true }
+    }
+}
 
+impl<T> Drop for IntoIter<T> {
+    fn drop(&mut self) {
+        unsafe {
+            for mut _i in 0..self.len {
+                if self.start >= self.end { 
+                    _i = self.len;
+                    continue;
+                }
+                
+                ptr::drop_in_place(self.start as *mut T);
+                self.start = self.start.offset(1);
+            }
+        }
+    }
+}
 
+impl<T> Drop for CustomVec<T> {
+    fn drop(&mut self) {
+        if self.len != 0 {
+            unsafe {
+                let slice: &mut [T] = core::slice::from_raw_parts_mut(self.ptr(), self.len);
+                
+                let copy_ptr: *mut T = alloc(Layout::array::<T>(self.len).unwrap()) as *mut T;
+                copy_ptr.copy_from_nonoverlapping(slice.as_ptr(), self.len);
+                let copy_slice: &mut [T] = core::slice::from_raw_parts_mut(copy_ptr, self.len);
+                _ = Box::from_raw(copy_slice as *mut [T]);
+            }
+        }
+    
+        // deallocation is handled by RawVec
+    }
+}
 
-pub struct CustomVec<T> {
+impl<T> Drop for RawVec<T> {
+    fn drop(&mut self) {
+        if self.cap != 0 {
+            let layout = Layout::array::<T>(self.cap).unwrap();
+            unsafe {
+                dealloc(self.ptr.as_ptr() as *mut u8, layout);
+            }
+        }
+    }
+}
+
+struct RawVec<T> {
     ptr: NonNull<T>,
     cap: usize,
-    len: usize,
 }
 
-pub struct IntoIter<T> {
-    buf: NonNull<T>,
-    cap: usize,
-    start: *const T,
-    end: *const T,
-    len: usize,
-}
-
-impl<T> CustomVec<T> {
-    pub fn new() -> Self {
-        assert!(mem::size_of::<T>() != 0, "We're not ready to handle ZSTs");
-        CustomVec {
+impl<T> RawVec<T> {
+    fn new() -> Self {
+        assert!(mem::size_of::<T>() != 0, "TODO: implement ZST support");
+        RawVec {
             ptr: NonNull::dangling(),
-            len: 0,
             cap: 0,
         }
     }
 
     fn grow(&mut self) {
-        let (new_cap, new_layout) = if self.cap == 0 {
-            (1, Layout::array::<T>(1).unwrap())
-        } else {
-            // This can't overflow since self.cap <= isize::MAX.
-            let new_cap = 2 * self.cap;
+        let new_cap = if self.cap == 0 { 1 } else { 2 * self.cap };
 
-            // `Layout::array` checks that the number of bytes is <= usize::MAX,
-            // but this is redundant since old_layout.size() <= isize::MAX,
-            // so the `unwrap` should never fail.
-            let new_layout = Layout::array::<T>(new_cap).unwrap();
-            (new_cap, new_layout)
-        };
+        let new_layout = Layout::array::<T>(new_cap).unwrap();
 
-        // Ensure that the new allocation doesn't exceed `isize::MAX` bytes.
         assert!(new_layout.size() <= isize::MAX as usize, "Allocation too large");
 
         let new_ptr = if self.cap == 0 {
@@ -298,74 +344,83 @@ impl<T> CustomVec<T> {
             let old_ptr = self.ptr.as_ptr() as *mut u8;
             unsafe { realloc(old_ptr, old_layout, new_layout.size()) }
         };
-
-        // If allocation fails, `new_ptr` will be null, in which case we abort.
         self.ptr = match NonNull::new(new_ptr as *mut T) {
             Some(p) => p,
             None => handle_alloc_error(new_layout),
         };
         self.cap = new_cap;
     }
+}
+
+pub struct CustomVec<T> {
+    buf: RawVec<T>,
+    len: usize,
+}
+
+impl<T> CustomVec<T> {
+    fn ptr(&self) -> *mut T {
+        self.buf.ptr.as_ptr()
+    }
+
+    fn cap(&self) -> usize {
+        self.buf.cap
+    }
+
+    pub fn new() -> Self {
+        CustomVec {
+            buf: RawVec::new(),
+            len: 0,
+        }
+    }
 
     pub fn push(&mut self, elem: T) {
-        if self.len == self.cap { self.grow(); }
+        if self.len == self.cap() { self.buf.grow(); }
+
         unsafe {
-            ptr::write(self.ptr.as_ptr().add(self.len), elem);
+            ptr::write(self.ptr().add(self.len), elem);
         }
-        // Can't fail, we'll OOM first.
+
         self.len += 1;
     }
-    
+
     pub fn pop(&mut self) -> Option<T> {
         if self.len == 0 {
             None
         } else {
             self.len -= 1;
             unsafe {
-                Some(ptr::read(self.ptr.as_ptr().add(self.len)))
+                Some(ptr::read(self.ptr().add(self.len)))
             }
         }
     }
 
     pub fn insert(&mut self, index: usize, elem: T) {
         assert!(index <= self.len, "index out of bounds");
-        if self.cap == self.len { self.grow(); }
+        if self.cap() == self.len { self.buf.grow(); }
     
         unsafe {
             ptr::copy(
-                self.ptr.as_ptr().add(index),
-                self.ptr.as_ptr().add(index + 1),
+                self.ptr().add(index),
+                self.ptr().add(index + 1),
                 self.len - index,
             );
-            ptr::write(self.ptr.as_ptr().add(index), elem);
+            ptr::write(self.ptr().add(index), elem);
             self.len += 1;
         }
     }
 
     pub fn remove(&mut self, index: usize) -> T {
         assert!(index < self.len, "index out of bounds");
-        
+
         unsafe {
             self.len -= 1;
-            let result = ptr::read(self.ptr.as_ptr().add(index));
+            let result = ptr::read(self.ptr().add(index));
             ptr::copy(
-                self.ptr.as_ptr().add(index + 1),
-                self.ptr.as_ptr().add(index),
+                self.ptr().add(index + 1),
+                self.ptr().add(index),
                 self.len - index,
             );
             result
-        }
-    }
-}
-
-impl<T> Drop for CustomVec<T> {
-    fn drop(&mut self) {
-        if self.cap != 0 {
-            unsafe {
-                let slice: &mut [T] = core::slice::from_raw_parts_mut(self.ptr.as_ptr(), self.len);
-                _ = Box::from_raw(slice as *mut [T]);
-            }
-            core::mem::forget(self.ptr);
         }
     }
 }
@@ -374,7 +429,7 @@ impl<T> Deref for CustomVec<T> {
     type Target = [T];
     fn deref(&self) -> &[T] {
         unsafe {
-            core::slice::from_raw_parts(self.ptr.as_ptr(), self.len)
+            core::slice::from_raw_parts(self.ptr(), self.len)
         }
     }
 }
@@ -382,31 +437,36 @@ impl<T> Deref for CustomVec<T> {
 impl<T> DerefMut for CustomVec<T> {
     fn deref_mut(&mut self) -> &mut [T] {
         unsafe {
-            core::slice::from_raw_parts_mut(self.ptr.as_ptr(), self.len)
+            core::slice::from_raw_parts_mut(self.ptr(), self.len)
         }
     }
 }
+
+pub struct IntoIter<T> {
+    _buf: RawVec<T>,
+    start: *const T,
+    end: *const T,
+    len: usize,
+}
+
 
 impl<T> IntoIterator for CustomVec<T> {
     type Item = T;
     type IntoIter = IntoIter<T>;
     fn into_iter(self) -> IntoIter<T> {
-        let vec: ManuallyDrop<CustomVec<T>> = ManuallyDrop::new(self);
-
-        let ptr: NonNull<T> = vec.ptr;
-        let cap: usize = vec.cap;
-        let len: usize = vec.len;
-
         unsafe {
+            let buf = ptr::read(&self.buf);
+            let len = self.len;
+            mem::forget(self);
+
             IntoIter {
-                buf: ptr,
-                cap: cap,
-                start: ptr.as_ptr(),
-                end: if cap == 0 {
-                    ptr.as_ptr()
+                start: buf.ptr.as_ptr(),
+                end: if buf.cap == 0 {
+                    buf.ptr.as_ptr()
                 } else {
-                    ptr.as_ptr().add(len)
+                    buf.ptr.as_ptr().add(len)
                 },
+                _buf: buf,
                 len: len,
             }
         }
@@ -419,10 +479,11 @@ impl<T> Iterator for IntoIter<T> {
         if self.start == self.end {
             None
         } else {
+            // self.len -= 1;
+
             unsafe {
                 let result: T = ptr::read(self.start);
                 self.start = self.start.offset(1);
-                self.len -= 1;
                 Some(result)
             }
         }
@@ -439,35 +500,17 @@ impl<T> DoubleEndedIterator for IntoIter<T> {
         if self.start == self.end {
             None
         } else {
+            // self.len -= 1;
             unsafe {
                 self.end = self.end.offset(-1);
-                self.len -= 1;
                 Some(ptr::read(self.end))
             }
         }
     }
 }
 
-impl<T> Drop for IntoIter<T> {
-    fn drop(&mut self) {
-        if self.cap != 0 {
-            if core::mem::needs_drop::<T>() {
-                unsafe {
-                    let start_ptr = self.start as *mut T;
-            
-                    for i in 0..self.len {
-                        let element_ptr = start_ptr.add(i);
-                        ptr::drop_in_place(element_ptr);
-                    }
-                }
-            }
-            core::mem::forget(self.start);
-            core::mem::forget(self.end);
+unsafe impl<T: Send> Send for CustomVec<T> {}
+unsafe impl<T: Sync> Sync for CustomVec<T> {}
 
-            let layout: Layout = Layout::array::<T>(self.cap).unwrap();
-            unsafe {
-                dealloc(self.buf.as_ptr() as *mut u8, layout);
-            }
-        }
-    }
-}
+unsafe impl<T: Send> Send for RawVec<T> {}
+unsafe impl<T: Sync> Sync for RawVec<T> {}

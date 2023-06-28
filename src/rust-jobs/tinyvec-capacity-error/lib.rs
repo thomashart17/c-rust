3051c17f1bbfe70a9ad0494a8b5e9802fbe1fa93
Thomas Hart <thomas.hart@uwaterloo.ca>
2023-06-28 17:29:25 -0400

    fix: convert to sat test

--- src/rust-jobs/tinyvec-capacity-error/lib.rs
+++ src/rust-jobs/tinyvec-capacity-error/lib.rs
@@ -6,7 +6,7 @@ use tinyvec::ArrayVec;
 
 #[no_mangle]
 pub extern "C" fn entrypt() {
-    let vec: ArrayVec<[u32; u16::MAX as usize]> = ArrayVec::new();
+    let vec: ArrayVec<[u32; u16::MAX as usize + 1]> = ArrayVec::new();
 
     sea::sassert!(vec.capacity() == u16::MAX as usize);
 }

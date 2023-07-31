5e8f597fe8724e0de35098a64a4c81dff157b61b
Thomas Hart <thomas.hart@uwaterloo.ca>
2023-07-31 13:40:36 -0400

    chore: add comments explaining tinyvec error jobs

--- src/rust-jobs/tinyvec-capacity-error/lib.rs
+++ src/rust-jobs/tinyvec-capacity-error/lib.rs
@@ -4,9 +4,13 @@ use sea;
 
 use tinyvec::ArrayVec;
 
+
+// Testing the error fixed here: https://github.com/Lokathor/tinyvec/pull/178
 #[no_mangle]
 pub extern "C" fn entrypt() {
     let vec: ArrayVec<[u32; u16::MAX as usize + 1]> = ArrayVec::new();
 
+    // When using previous versions of the library where the error is still present,
+    // this assertion should fail. For newer versions, it should pass.
     sea::sassert!(vec.capacity() == u16::MAX as usize);
 }

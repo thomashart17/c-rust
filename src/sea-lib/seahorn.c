39d1f7c988db08d0e6fb42611676d81918203cba
LiangLiWSL <xingliang2002@gmail.com>
2023-09-22 17:56:10 -0400

    revert bcmp function

--- src/sea-lib/seahorn.c
+++ src/sea-lib/seahorn.c
@@ -58,32 +58,30 @@ void *realloc(void *ptr, size_t sz) { return sea_realloc(ptr, sz); }
 void sea_printf( const char* format, ... ) { return; }
 
 INLINE int bcmp(const void *s1, const void *s2, size_t n) {
-  // size_t i;
+  size_t i;
 
-  // const size_t max_buffer_size = 32;
-  // const uint8_t *p1;
-  // const uint8_t *p2;
-  // p1 = s1;
-  // p2 = s2;
+  const size_t max_buffer_size = 32;
+  const uint8_t *p1;
+  const uint8_t *p2;
+  p1 = s1;
+  p2 = s2;
 
-  // if (p1 == p2)
-  //   return 0;
-  // if (p1 == NULL || p2 == NULL)
-  //   return 1;
+  if (p1 == p2)
+    return 0;
+  if (p1 == NULL || p2 == NULL)
+    return 1;
   // /* pre-unroll the loop for MAX_BUFFER_SIZE */
-  // #pragma unroll 32
-  // for (i = 0; i < max_buffer_size; i++) {
-  //   if (i < n) {
-  //     if (p1[i] != p2[i]) {
-  //       return 1;
-  //     }
-  //   }
-  // }
+  #pragma unroll 32
+  for (i = 0; i < max_buffer_size; i++) {
+    if (i < n) {
+      if (p1[i] != p2[i]) {
+        return 1;
+      }
+    }
+  }
   /* unroll the rest, if any (doesn't really work) */
-  // for (i = max_buffer_size; i < n; i++) {
-  //   if (p1[i] != p2[i])
-  //     return 1;
-  // }
-
-  return 1;
+  for (i = max_buffer_size; i < n; i++) {
+    if (p1[i] != p2[i])
+      return 1;
+  }
 }
